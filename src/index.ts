4b8e3075230481c0836af38d08f52dae1499c945
Mohamed EL-Raghy <elraghy8@gmail.com>
2023-10-23 22:26:09 +0300

    create Linked List class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedListCollection";
 
 // const numbersCollection = new NumbersCollection([-10000, 30, -4, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log("sorted arr", numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("ZzZaAXxX");
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection("ZzZaAXxX");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log("sorted string", charactersCollection.data);
+
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log("sorted string", charactersCollection.data);
+// console.log(linkedList.length);
+linkedList.print();
